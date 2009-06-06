cc121348885b83877500d0e62ad598c246e2cc5c
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@d76f907e-3ff3-11de-9b50-8bb2048eb44c>
2009-06-06 15:00:14 +0000

    MOV: Moved the individual folders into the hydrazine directory.

---
+++ hydrazine/test/TestActiveTimer.h
@@ -0,0 +1,50 @@
+/*!
+
+	\file TestActiveTimer.h
+	
+	\date September 25, 2008
+	
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	
+	\brief The header file for the TestActiveTimer class.
+
+*/
+
+#ifndef TEST_ACTIVE_TIMER_H_INCLUDED
+#define TEST_ACTIVE_TIMER_H_INCLUDED
+
+#include <hydrazine/interface/Test.h>
+#include <hydrazine/interface/ActiveTimer.h>
+#include <boost/random/mersenne_twister.hpp>
+
+namespace test
+{
+	class TestActiveTimer : public Test
+	{
+		private:
+			class Timer : public hydrazine::ActiveTimer
+			{
+				public:
+					hydrazine::Timer timer;
+					void fired();
+			
+			};
+
+		private:
+			bool doTest( );
+		
+		public:
+			TestActiveTimer();
+			
+			unsigned int objects;
+			hydrazine::Timer::Second delayStep;
+			hydrazine::Timer::Second minDelay;
+	
+	};
+
+}
+
+int main( int argc, char** argv );
+
+#endif
+
---
+++ hydrazine/test/TestPackage.h
@@ -0,0 +1,103 @@
+/*!
+
+	\file TestPackage.h
+	
+	\date Friday Novermber 7, 2008
+	
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	
+	\brief The header file for the TestPackage class
+
+*/
+
+#ifndef TEST_PACKAGE_H_INCLUDED
+#define TEST_PACKAGE_H_INCLUDED
+
+#include <hydrazine/implementation/Package.h>
+#include <hydrazine/interface/Test.h>
+#include <unordered_set>
+#include <vector>
+
+namespace test
+{
+
+	/*!
+	
+		\brief A test for the Package class.
+		
+		TestPoints:
+		
+			1) Add some variables, copy the package, remove the variables, 
+				make sure that they match.
+		
+			2) Randomly add, remove, reserve, and compress the package.  Finally
+				remove all variables and make sure that they match.
+	
+	*/
+	class TestPackage : public Test
+	{
+	
+		private:
+				
+			class Packable : public hydrazine::Packable
+			{
+			
+				private:
+				
+					uint8_t* _begin;
+					uint8_t* _end;
+				
+				public:
+			
+					Packable();
+					Packable( const Packable& packable );
+					~Packable();
+
+					const Packable& operator=( const Packable& packable ); 
+					
+					void resize( unsigned int bytes );
+					
+					unsigned int bytes() const;
+					unsigned int size() const;
+
+					uint8_t& operator[]( unsigned int index );
+					void write( uint8_t* data, unsigned int size );
+
+					bool operator==( const Packable& packable ) const;
+
+					void pack( void* package );
+					void unpack( void* package );
+					
+					unsigned int sum() const;
+			
+			};
+
+			typedef std::vector< Packable > PackageVector;
+			typedef std::unordered_set< unsigned int > PackableSet;
+	
+		private:
+		
+			bool _testCopy();
+			bool _testComplex();
+
+			bool doTest();
+	
+		public:
+	
+			TestPackage();
+			
+			unsigned int reads;
+			unsigned int writes;
+			unsigned int averageSize;
+			unsigned int objects;
+			unsigned int adds;
+	
+	};
+
+
+}
+
+int main( int argc, char** argv );
+
+#endif
+
