df58e819b6ab1cbff430b298799bc437c374915b
Gregory Diamos <solusstultus@gmail.com>
2009-07-02 15:15:38 +0000

    ADD: Added device querying functions.  ENH: updated debug to make the display format more readable for reporting macros.

--- hydrazine/implementation/debug.cpp
+++ hydrazine/implementation/debug.cpp
@@ -22,6 +22,28 @@ namespace hydrazine
 		\brief Global report timer
 	*/
 	Timer _ReportTimer;
+	
+	std::string _debugTime()
+	{
+		std::stringstream stream;
+		stream.setf( std::ios::fixed, std::ios::floatfield );
+		stream.precision( 6 );
+		stream << _ReportTimer.seconds();
+		return stream.str();
+	}
+	
+	std::string _debugFile( const std::string& file, unsigned int line )
+	{
+		std::stringstream lineColon;
+		lineColon << line << ":";
+		
+		std::stringstream stream;
+		stream << stripReportPath( file ) << ":";
+		stream.width( 5 );
+		stream.fill( ' ' );
+		stream << std::left << lineColon.str();
+		return stream.str();
+	}
 
 }
 #endif
