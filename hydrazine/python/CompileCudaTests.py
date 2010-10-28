5dc72593f63dce09afcc8c2c99778ab07956d9f3
Andrew Kerr <arkerr@gmail.com>
2010-10-28 18:26:00 +0000

    added a scraper for CUDA SDK Makefiles

--- hydrazine/python/CompileCudaTests.py
+++ hydrazine/python/CompileCudaTests.py
@@ -9,6 +9,7 @@
 
 import os
 from optparse import OptionParser
+import re
 
 class CudaSource:
 	def __init__(self, path, ptx):
@@ -32,10 +33,87 @@ def getAllCudaSources(path, ptx):
 					sources.append(CudaSource(name, ptx))
 	return sources
 
+def printAutomakeBlock(dirpath, sources):
+	exe = sources['exe'][0][0:1].capitalize() + sources['exe'][0][1:]
+	cppFiles = [os.path.join(dirpath,x) for x in sources['cpp'] ]
+	cudaFiles = [os.path.join(dirpath, x + '.cpp') for x in sources['cuSources'] ]
+	print "################################################################################"
+	print "##", exe
+	print exe + '_CXXFLAGS = $(CUDA_CFLAGS) $(SDK_CFLAGS)'
+	print exe + '_SOURCES = ' + ' \\\n\t'.join(cppFiles + cudaFiles)
+	print exe + '_LDADD = $(OCELOT_LIBS)'
+	print exe + '_LDFLAGS = -static'
+	print "################################################################################\n"
+	return exe
+
+# Obtain everything up to a token
+#
+# [x.strip("") for x in re.split("\s", line) if x.strip() != '']
+#
+def splitLineUpToComment(line):
+	tokens = [x.strip("") for x in re.split("\s", line) if x.strip() != '']
+	result = []
+	for token in tokens:
+		if token != '#':
+			result.append(token)
+		else:
+			break;
+	return result
+
+#
+#
+def getAllCudaSourcesMakefile(path, ptx, automake=False, apps=False):
+	""" scrapes CUDA dependencies by examining a Makefile located in the application's source directory """
+	cuFiles = []		
+	programs = []
+	for dirpath, dirnames, filenames in os.walk(path):
+		for filename in filenames:
+			name = os.path.join(dirpath, filename)
+			if os.path.isfile(name) and os.path.basename(name) == 'Makefile':
+				sources = {'cpp': [], 'cuDeps': [], 'cuSources': [], 'exe': []}
+				tokenStateMap = {'CCFILES': 'cpp', 'CFILES': 'cpp', 'CUDEPS': 'cuDeps', 'CU_DEPS': 'cuDeps', \
+					'CUFILES': 'cuSources', 'CUFILES_sm_10': 'cuSources', 'CUFILES_sm_11': 'cuSources', \
+					'CUFILES_sm_20': 'cuSources', 'CUFILES_sm_12': 'cuSources', 'CUFILES_sm_13': 'cuSources', \
+					'END' :'none', 'MPICXX': 'none', 'EXECUTABLE': 'exe'}
+				tokens = []
+				lines = [x for x in open(os.path.abspath(name), "r").readlines()]
+				for line in lines:
+					if line[0] == '#':
+						if len(tokens) == 0 or tokens[-1] != 'END':
+							tokens.append('END')
+					else:
+						tokens += splitLineUpToComment(line)
+				state = 'none'
+				for token in tokens:
+					if token in tokenStateMap.keys():
+						state = tokenStateMap[token]
+					else:
+						regex = """[/0-9\-a-zA-Z_]+\.[a-z]+"""
+						if state == 'exe' and token != 'EXECUTABLE':
+							regex = """[/0-9\-a-zA-Z_]+"""
+						g = re.search(regex, token)
+						if g and state in sources.keys():
+							sources[state].append(g.group(0))
+				cuFiles += [CudaSource(os.path.join(dirpath,x), ptx) for x in sources['cuSources']]
+				
+				#
+				# print Automake components while we're at it
+				#
+				if len(sources['exe']) and automake:
+					programs.append(printAutomakeBlock(dirpath, sources))
+					
+	if apps and automake and len(programs):
+		print "check_PROGRAMS =", " \\\n\t".join(programs)
+	return cuFiles
+
+#
+#
+#
 def compileSources(commandBase, sources, continueOnError):
 	for source in sources:
 		if not os.path.isfile(source.outfile):
-			command = commandBase + " -I" + os.path.dirname(source.filename) \
+			command = commandBase + " -I" + os.path.dirname(source.filename) + " -I" + os.path.dirname(source.filename) + "/../inc"\
+				+ " -I./inc -Isdk -Ishared " \
 				+ " -o " + source.outfile + " " + source.filename
 			print command
 			os.system(command)
@@ -77,6 +155,12 @@ def main():
 		dest="clean", default=False, help="Delete all .cu.cpp files.")
 	parser.add_option("-s", "--sanitize", action="store_true", 
 		dest="sanitize", default=False, help="Only sanitize .cu.cpp files.")
+	parser.add_option("-m", "--makefile", action="store_true",
+		dest="makefile", help="Print components of Makefile.am for each application to stdout")
+	parser.add_option("-L", "--list", action="store_true",
+		dest="listapps", help="Print a listing of applications for Makefile.am")
+	parser.add_option("-n", "--nocompile", action="store_true",
+		dest="nocompile", help="Do not compile")
 	
 	(options, args) = parser.parse_args()
 	
@@ -85,7 +169,12 @@ def main():
 	else:
 		command = "nvcc --cuda " + options.arguments
 	path = os.getcwd()
-	sources = getAllCudaSources(options.directory, options.ptx)
+	
+	if options.makefile:
+		sources = getAllCudaSourcesMakefile(options.directory, options.ptx, options.makefile, options.listapps)
+	else:
+		sources = getAllCudaSources(options.directory, options.ptx)
+	
 	if options.ptx:
 		compileSources(command, sources, True)
 	else:
@@ -94,8 +183,10 @@ def main():
 		elif options.sanitize:
 			sanitizeSources(sources)
 		else:
-			compileSources(command, sources, False)
-			sanitizeSources(sources)
+			if not options.nocompile:
+				compileSources(command, sources, False)
+			if not options.makefile:
+				sanitizeSources(sources)
 	
 if __name__ == "__main__":
     main()
