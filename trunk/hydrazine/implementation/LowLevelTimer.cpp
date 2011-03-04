854bf6c6e969378d82e7cda7c69846d1aa2afb8a
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@d76f907e-3ff3-11de-9b50-8bb2048eb44c>
2011-03-04 15:07:24 +0000

    ENH: Added code path to low level timer to support Mac API calls. Thanks to Andrew Corrigan for the path.

--- trunk/hydrazine/implementation/LowLevelTimer.cpp
+++ trunk/hydrazine/implementation/LowLevelTimer.cpp
@@ -12,6 +12,8 @@
 
 #ifdef WIN32
 	#include <windows.h>
+#elif __APPLE__
+	#include <mach/mach_time.h>
 #endif
 
 #include <hydrazine/implementation/LowLevelTimer.h>
@@ -86,13 +88,26 @@ namespace hydrazine
 			QueryPerformanceFrequency((LARGE_INTEGER*) &frequency);
 			QueryPerformanceCounter((LARGE_INTEGER*) &cycles);
 			return cycles / frequency;
+
+#elif __APPLE__ 
+            uint64_t absolute_time = mach_absolute_time();
+            mach_timebase_info_data_t info = {0,0};
+            
+            if (info.denom == 0) mach_timebase_info(&info);
+            uint64_t elapsednano = absolute_time * (info.numer / info.denom);
+            
+            timespec spec;
+            spec.tv_sec = elapsednano * 1e-9;
+            spec.tv_nsec = elapsednano - (tp->tv_sec * 1e9);
+			return spec.tv_nsec + (Cycle)spec.tv_sec * 1e9;
 #else
 			timespec spec;
 		
 			clock_gettime( CLOCK_REALTIME, &spec );
-		
-			return spec.tv_nsec + (Cycle)spec.tv_sec * 1000000000;
+
+			return spec.tv_nsec + (Cycle)spec.tv_sec * 1e9;
 #endif
+		
 		}
 	}
 }
