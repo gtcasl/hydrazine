cc911a54ebbd32ce1285a384edb6f7e91abffa19
Gregory Diamos <solusstultus@gmail.com>
2011-03-10 02:45:52 +0000

    DEL: Removed legacy cuda support.  MOV: Changed windows compatbility to system compatibility.

--- hydrazine/interface/SystemCompatibility.h
+++ hydrazine/interface/SystemCompatibility.h
@@ -1,12 +1,12 @@
-/*! \file WindowsCompatibility.h
+/*! \file SystemCompatibility.h
 	\date Monday August 2, 2010
 	\author Gregory Diamos <gregory.diamos@gatech.edu>
 	\brief The header file for hacked code required to assist windows 
 		compilaiton
 */
 
-#ifndef WINDOWS_COMPATIBILITY_H_INCLUDED
-#define WINDOWS_COMPATIBILITY_H_INCLUDED
+#ifndef SYSTEM_COMPATIBILITY_H_INCLUDED
+#define SYSTEM_COMPATIBILITY_H_INCLUDED
 
 #include <unordered_map>
 #include <unordered_set>
@@ -19,13 +19,13 @@
 #ifdef WIN32
 namespace std
 {
-	template< typename T1, typename T2 >
-	class unordered_map : public std::tr1::unordered_map< T1, T2 > {};	
+	template<typename T1, typename T2>
+	class unordered_map : public std::tr1::unordered_map<T1, T2> {};	
 
-	template< typename T1 >
-	class unordered_set : public std::tr1::unordered_set< T1 > {};
+	template<typename T1>
+	class unordered_set : public std::tr1::unordered_set<T1> {};
 
-	template< typename T1 >
+	template<typename T1>
 	T1 move(const T1& t) { return t; }
 }
 
@@ -34,7 +34,10 @@ namespace std
 
 namespace hydrazine
 {
+	/*! \brief Get the number of hardware threads */
 	unsigned int getHardwareThreadCount();
+	/*! \brief Get the full path to the named executable */
+	std::string getExecutablePath(const std::string& executableName);
 }
 
 #endif
