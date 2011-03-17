0dcf6630fa6e227e0cbe6c33bb4adbbedacbf5a9
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@d76f907e-3ff3-11de-9b50-8bb2048eb44c>
2011-03-17 20:46:04 +0000

    DEL: Removed include for std::string in system compatibility.  ENH: Added skeleton for an elf file reader.

---
+++ hydrazine/implementation/ELFFile.cpp
@@ -0,0 +1,8 @@
+/* 	\file   ELF.h
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\date   Thursday March 17, 2011
+	\brief  The header file containing ELF file definitions.
+	
+	See the document 'ELF-64 Object File Format Version 1.5 Draft 2 (May 1998)'
+	for details.
+*/
