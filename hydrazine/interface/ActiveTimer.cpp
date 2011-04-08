6f6ad164a1b56437d1cb341c04a1ac189ce56d75
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@d76f907e-3ff3-11de-9b50-8bb2048eb44c>
2011-04-08 02:29:50 +0000

    ENH: Removed references to pthreads from active message..

--- hydrazine/interface/ActiveTimer.cpp
+++ hydrazine/interface/ActiveTimer.cpp
@@ -26,87 +26,65 @@
 namespace hydrazine
 {
 
-	////////////////////////////////////////////////////////////////////////////			
+	////////////////////////////////////////////////////////////////////////////
 	// ActiveTimer::SharedData
 
 	ActiveTimer::SharedData ActiveTimer::_sharedData;
 
 	ActiveTimer::SharedData::SharedData()
 	{
-	
 		report( "Creating shared data." );
-		pthread_mutex_init( &mutex, 0 );
-		pthread_cond_init( &condition, 0 );
-		pthread_attr_init( &attribute );
-		pthread_attr_setstacksize( &attribute, ACTIVE_TIMER_STACK_SIZE );
-		pthread_attr_setdetachstate( &attribute, PTHREAD_CREATE_DETACHED );		
 		thread = 0;
 		connections = 0;
 		alive = false;
 		timer.start();
-	
 	}
 	
 	ActiveTimer::SharedData::~SharedData()
 	{
-	
 		report( "Destroying shared data." );
 		assert( connections == 0 );
-		pthread_mutex_destroy( &mutex );
-		pthread_cond_destroy( &condition );
-		pthread_attr_destroy( &attribute );
-	
 	}
 	
 	void ActiveTimer::SharedData::next()
 	{
-	
-		pthread_mutex_lock( &mutex );
+		mutex.lock();
 
 		//report( " Locked mutex, firing expired timers." );
-		for( TimerMap::iterator fi = timers.begin(); fi != timers.end(); )
+		for( TimerMap::iterator fi = timers.begin(); fi != timers.end();)
 		{
-		
 			if( fi->first < timer.seconds() )
 			{
-		
 				fi->second->_done = true;
 				fi->second->fired();
-				pthread_cond_signal( &fi->second->_condition );
+				fi->second->_condition.notify_all();
 				report( "  Fired ActiveTimer " << fi->second
 					<< " at " << fi->first );
 				TimerMap::iterator next = fi;
 				++next;
 				timers.erase( fi );
 				fi = next;
-		
 			}
 			else
 			{
-		
-				//report( "  Checked ActiveTimer " << fi->second.second << 
-				//	", remaining time is " 
-				//	<< ( fi->first - timer.seconds() ) );
 				++fi;
-		
 			}
-		
 		}
 				
 		if( timers.empty() )
 		{
 		
 			//report( " No active timers, waiting." );		
-			pthread_cond_wait( &condition, &mutex );
+			condition.notify_all();
 			//report( " Unlocking mutex." );
-			pthread_mutex_unlock( &mutex );
+			mutex.unlock();
 		
 		}
 		else
 		{
 		
 			//report( " Unlocking mutex, yielding." );
-			pthread_mutex_unlock( &mutex );
+			mutex.unlock();
 			pthread_yield();
 		
 		}
@@ -118,17 +96,17 @@ namespace hydrazine
 	
 		report( "Thread is alive." );
 		ActiveTimer::SharedData& data = ActiveTimer::_sharedData;
-		pthread_mutex_lock( &data.mutex );
+		boost::unique_lock<boost::mutex> lock(data.mutex);
 		report( " Locked mutex" );
 		assert( !data.alive );
 		assert( argument == 0 );
 		data.alive = true;
 		report( " Signalled alive" );
-		pthread_cond_signal( &data.condition );
+		data.condition.notify_all();;
 		report( " Signalled creator, waiting for signal." );
-		pthread_cond_wait( &data.condition, &data.mutex );
+		data.condition.wait(lock);
 		report( " Signal received, unlocking mutex." );
-		pthread_mutex_unlock( &data.mutex );
+		lock.unlock();
 		
 		while( data.connections > 0 )
 		{
@@ -140,9 +118,8 @@ namespace hydrazine
 		assert( data.timers.empty() );
 		report( " Thread is dying." );
 		data.alive = false;
-		pthread_cond_signal( &data.condition );
+		data.condition.notify_all();
 		report( " Thread is dead." );
-		pthread_exit( argument );
 		
 		return 0;
 	}
@@ -152,43 +129,39 @@ namespace hydrazine
 	// ActiveTimer
 	ActiveTimer::ActiveTimer()
 	{
-	
 		report( "Creating new ActiveTimer " << this );
-		pthread_cond_init( &_condition, 0 );
-		pthread_mutex_lock( &_sharedData.mutex );
+		boost::unique_lock<boost::mutex> lock( _sharedData.mutex );
 		++_sharedData.connections;
 		
 		if( _sharedData.connections == 1 )
 		{
 		
 			assert( _sharedData.thread == 0 );
-			pthread_create( &_sharedData.thread, &_sharedData.attribute, 
-				SharedData::run, 0 );
+			
+			_sharedData.thread = new boost::thread( SharedData::run, this );
 			report( " Waiting on condition " << this );
-			pthread_cond_wait( &_sharedData.condition, &_sharedData.mutex );
+			_sharedData.condition.wait( lock );
 			report( " Signalling " << this );
-			pthread_cond_signal( &_sharedData.condition );
+			_sharedData.condition.notify_all();
 		
 		}
 		
 		assert( _sharedData.alive );
-		pthread_mutex_unlock( &_sharedData.mutex );
+		lock.unlock();
 		_done = true;
-	
 	}
 	
 	ActiveTimer::~ActiveTimer()
 	{
-	
 		report( "Destroying ActiveTimer " << this );
-		pthread_mutex_lock( &_sharedData.mutex );
+		boost::unique_lock<boost::mutex> lock( _sharedData.mutex );
 		assert( _sharedData.alive );
 		
 		if( !_done )
 		{
 		
 			report("Timer not finished, waiting for signal.");
-			pthread_cond_wait( &_condition, &_sharedData.mutex );
+			_sharedData.condition.wait( lock );
 		
 		}		
 		
@@ -197,40 +170,38 @@ namespace hydrazine
 		if( _sharedData.connections == 0 )
 		{
 		
-			pthread_cond_signal( &_sharedData.condition );
-			pthread_cond_wait( &_sharedData.condition, &_sharedData.mutex );
+			_sharedData.condition.notify_all();
+			_sharedData.condition.wait( lock );
 			assert( !_sharedData.alive );
+			delete _sharedData.thread;
 			_sharedData.thread = 0;
 			
 		}
 		
-		pthread_mutex_unlock( &_sharedData.mutex );
-		pthread_cond_destroy( &_condition );
+		lock.unlock();
 		report( "Destroyed ActiveTimer " << this );
-	
 	}
 
 	ActiveTimer::ActiveTimer( const ActiveTimer& timer )
 	{
 	
 		report( "Creating new ActiveTimer " << this << " from " << &timer );
-		pthread_cond_init( &_condition, 0 );
-		pthread_mutex_lock( &_sharedData.mutex );
+		
+		boost::unique_lock<boost::mutex> lock( _sharedData.mutex );
 		++_sharedData.connections;
 		
 		if( _sharedData.connections == 1 )
 		{
 		
 			assert( _sharedData.thread == 0 );
-			pthread_create( &_sharedData.thread, &_sharedData.attribute, 
-				SharedData::run, 0 );
-			pthread_cond_wait( &_sharedData.condition, &_sharedData.mutex );
-			pthread_cond_signal( &_sharedData.condition );
+			_sharedData.thread = new boost::thread( SharedData::run, this );
+			_sharedData.condition.wait( lock );
+			_sharedData.condition.notify_all();
 		
 		}
 		
 		assert( _sharedData.alive );
-		pthread_mutex_unlock( &_sharedData.mutex );
+		lock.unlock();
 		_done = true;
 	
 	}
@@ -246,12 +217,12 @@ namespace hydrazine
 	void ActiveTimer::start( Timer::Second seconds )
 	{
 	
-		pthread_mutex_lock( &_sharedData.mutex );
+		boost::unique_lock<boost::mutex> lock( _sharedData.mutex );
 		
 		if( _sharedData.timers.empty() )
 		{
 		
-			pthread_cond_signal( &_sharedData.condition );
+			_sharedData.condition.notify_all();;
 		
 		}
 		
@@ -259,23 +230,23 @@ namespace hydrazine
 		seconds += _sharedData.timer.seconds();
 		_sharedData.timers.insert( std::make_pair( seconds, this ) );
 		
-		pthread_mutex_unlock( &_sharedData.mutex );
+		lock.unlock();
 	
 	}
 
 	void ActiveTimer::wait()
 	{
 	
-		pthread_mutex_lock( &_sharedData.mutex );
+		boost::unique_lock<boost::mutex> lock( _sharedData.mutex );
 		
 		if( !_done )
 		{
 			
-			pthread_cond_wait( &_condition, &_sharedData.mutex );
+			_condition.wait( lock );
 		
 		}
 		
-		pthread_mutex_unlock( &_sharedData.mutex );
+		lock.unlock();
 	
 	}
 	////////////////////////////////////////////////////////////////////////////
